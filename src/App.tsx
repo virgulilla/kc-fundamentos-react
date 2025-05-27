cae856f7e602621c1d61bc1b0c9b0861a18e2482
oscar <oscar.caasmontes@gmail.com>
2025-05-27 19:50:50 +0200

    carga dinamica de componentes con suspense mostrando fallback squeeletton

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "./pages/auth/context";
-import LoginPage from "./pages/auth/login-page";
 import RegisterPage from "./pages/register/register-page";
 import NewAdvertPage from "./pages/advert/new-advert-page";
 import AdvertsPage from "./pages/advert/adverts-page";
 import NotFoundPage from "./pages/advert/not-found-page";
 import AdvertPage from "./pages/advert/advert-page";
 import Layout from "./components/layout/layout";
-import type { ReactNode } from "react";
+import { Suspense, lazy, type ReactNode } from "react";
+const LoginPage = lazy(() => import("./pages/auth/login-page"));
 
 interface AuthRouteProps {
   children: ReactNode;
@@ -29,6 +29,35 @@ const AuthRoute = ({ children, requireAuth, redirectTo }: AuthRouteProps) => {
   );
 };
 
+function LoginSkeleton() {
+  return (
+    <div className="bg-background flex min-h-screen items-center justify-center">
+      <div className="w-full max-w-sm animate-pulse space-y-6 rounded bg-white p-8 shadow-md">
+        <div className="mx-auto h-6 w-32 rounded bg-gray-300" />
+
+        <div className="h-10 w-full rounded border bg-red-100" />
+
+        <div className="space-y-2">
+          <div className="h-4 w-32 rounded bg-gray-300" />
+          <div className="h-10 w-full rounded bg-gray-300" />
+        </div>
+
+        <div className="space-y-2">
+          <div className="h-4 w-24 rounded bg-gray-300" />
+          <div className="h-10 w-full rounded bg-gray-300" />
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <div className="h-4 w-4 rounded bg-gray-300" />
+          <div className="h-4 w-24 rounded bg-gray-300" />
+        </div>
+
+        <div className="h-10 w-full rounded bg-gray-300" />
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Routes>
@@ -37,7 +66,9 @@ function App() {
           index
           element={
             <AuthRoute requireAuth={false}>
-              <LoginPage />
+              <Suspense fallback={<LoginSkeleton />}>
+                <LoginPage />
+              </Suspense>
             </AuthRoute>
           }
         />
